b61b4f15c766176145e9ec22260f5355762e9111
Ahsath <ahsath@gmail.com>
2019-06-26 18:09:54 -0500

    Added meta

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -20,7 +20,65 @@ import TheFooter from "~/layouts/TheFooter.vue";
 
 export default {
   metaInfo: {
-    title: "Hello, world!"
+    htmlAttrs: {
+      lang: "es"
+    },
+    title: "Aplicaciones Móviles y Web en tiempo récord",
+    meta: [
+      {
+        name: "title",
+        content: "inkntint",
+        template: chunk =>
+          `${chunk} - Aplicaciones Móviles y Web en tiempo récord`
+      },
+      {
+        name: "description",
+        content:
+          "Construimos aplicaciones móviles multiplataforma para Android, iOS y Escritorio de alto rendimiento desde el mismo código fuente"
+      },
+      // og - facebook
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://inkntint.com/" },
+      {
+        property: "og:title",
+        content: "inkntint",
+        template: chunk =>
+          `${chunk} - Aplicaciones Móviles y Web en tiempo récord`
+      },
+      {
+        property: "og:description",
+        content:
+          "Construimos aplicaciones móviles multiplataforma para Android, iOS y Escritorio de alto rendimiento desde el mismo código fuente"
+      },
+      {
+        property: "og:image",
+        content: "htpps://inkntint.com/og-image.jpg"
+      },
+      // twitter
+      {
+        property: "twitter:card",
+        content: "summary_large_image"
+      },
+      {
+        property: "twitter:url",
+        content: "htpps://inkntint.com/"
+      },
+      {
+        property: "twitter:title",
+        content: "inkntint",
+        template: chunk =>
+          `${chunk} - Aplicaciones Móviles y Web en tiempo récord`
+      },
+      {
+        property: "twitter:description",
+        content:
+          "Construimos aplicaciones móviles multiplataforma para Android, iOS y Escritorio de alto rendimiento desde el mismo código fuente"
+      },
+      {
+        property: "twitter:image",
+        content: "htpps://inkntint.com/og-image.jpg"
+      }
+    ]
   },
   components: {
     TheHeader,
