1a3566ff1d4a6e2b4ff1a458180eb114f91c986c
Ahsath <ahsath@gmail.com>
2019-06-26 23:41:20 -0500

    Added spiritjs cdn + spirit animation

--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ import InkContainer from '~/components/InkContainer.vue';
 import InkGrid from '~/components/InkGrid.vue';
 
 export default function (Vue, { router, head, isClient }) {
+  head.script.push({
+    src: 'https://unpkg.com/spiritjs/dist/spirit.min.js',
+    body: true
+  })
+  head.script.push({
+    src: './spirit.js',
+    body: true
+  })
   // Set default layout as a global component
   Vue.component('InkContainer', InkContainer)
   Vue.component('InkGrid', InkGrid)
