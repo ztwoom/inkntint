1f14c29c25786f0467a46e6de08c66c428a7691f
Ahsath <ahsath@gmail.com>
2019-06-27 11:05:15 -0500

    Updated

--- src/main.js
+++ src/main.js
@@ -1,16 +1,5 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { config, library } from '@fortawesome/fontawesome-svg-core';
-import { faPhoneAlt, faEnvelope, faCheckCircle, faCopyright } from '@fortawesome/free-solid-svg-icons';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-
-config.autoAddCss = true;
-library.add(faPhoneAlt, faEnvelope, faWhatsapp, faCheckCircle, faCopyright);
-import '@fortawesome/fontawesome-svg-core/styles.css';
-
 import './assets/main.css';
 import 'typeface-lato';
 import InkContainer from '~/components/InkContainer.vue';
@@ -26,7 +15,6 @@ export default function (Vue, { router, head, isClient }) {
     body: true
   })
   // Set default layout as a global component
-  Vue.component('font-awesome', FontAwesomeIcon)
   Vue.component('InkContainer', InkContainer)
   Vue.component('InkGrid', InkGrid)
 }
