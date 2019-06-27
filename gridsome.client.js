693257ac9c3866dc210fb9e82344030647f78c10
Ahsath <ahsath@gmail.com>
2019-06-27 11:05:06 -0500

    Set autoAddCss = true so it wont purge

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,12 +1,12 @@
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-// import { config, library } from '@fortawesome/fontawesome-svg-core';
-// import { faPhoneAlt, faEnvelope, faCheckCircle, faCopyright } from '@fortawesome/free-solid-svg-icons';
-// import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-// import '@fortawesome/fontawesome-svg-core/styles.css';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import { faPhoneAlt, faEnvelope, faCheckCircle, faCopyright } from '@fortawesome/free-solid-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
-// config.autoAddCss = false;
-// library.add(faPhoneAlt, faEnvelope, faWhatsapp, faCheckCircle, faCopyright);
+config.autoAddCss = true;
+library.add(faPhoneAlt, faEnvelope, faWhatsapp, faCheckCircle, faCopyright);
 
-// export default function (Vue) {
-//     Vue.component('font-awesome', FontAwesomeIcon)
-// };
+export default function (Vue) {
+    Vue.component('font-awesome', FontAwesomeIcon)
+};
